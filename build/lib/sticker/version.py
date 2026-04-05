5b08db17bc13a52f99fdd759aa72f6576d25356f
archussy <leonardstach@gmail.com>
2026-04-05 21:18:19 +0200

    valepack

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f2ac21a7'
-version = '0.1.0+dev.f2ac21a7'
-linkified_version = '0.1.0+dev.[f2ac21a7](https://github.com/maunium/stickerpicker/commit/f2ac21a7ee34064b2f8da49ce18604223d54f97e)'
+git_revision = '98a2da81'
+version = '0.1.0+dev.98a2da81'
+linkified_version = '0.1.0+dev.[98a2da81](https://github.com/maunium/stickerpicker/commit/98a2da8181d884dc313ce4a6e3b1276b1837caf6)'
